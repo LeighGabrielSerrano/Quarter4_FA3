a6329d1dc04adcbaff6148d1efa696672f7efa65
LeighGabrielSerrano <leighgabriel.serrano@smc.pshs.edu.ph>
2025-05-08 01:23:24 +0800

    Update script.js

--- files/script.js
+++ files/script.js
@@ -9,12 +9,10 @@ function start() {
     let show = "Number inputted: " + num + "<br><br>";
 
     if (num % 2 === 1) {
-        // Odd number: repeat number num times
         for (let i = num; i >= 1; i--) {
             show += (i + " ").repeat(num).trim() + "<br>";
         }
     } else {
-        // Even number: repeat number i times, optional spacing
         for (let i = num; i >= 1; i--) {
             show += (i + " ").repeat(i).trim() + "<br>";
         }
